04544e4fdbfb40404ad758db3fa0249d9628b79f
lomon23 <lomontav22@gmail.com>
2025-09-09 00:41:37 +0300

    added sword model

--- entity/player.py
+++ entity/player.py
@@ -2,18 +2,27 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 
+
 class Player(FirstPersonController):
-    def __init__(self, position = (0,1,0)):
+    def __init__(self, position=(0, 1, 0)):
         super().__init__()
         self.position = position
-        #self.model = "cube",
-        #self.color = color.red, 
         self.speed = 10
-        
-        
 
-    #def upadate(self):
-    #    if held_keys['a']:
-    #        self.rotation_y -= 2
-    #    if held_keys['d']:
-    #        self.rotation_y += 2
+        # Модель та колір гравця (можна розкоментувати для тестів)
+        # self.model = 'cube'
+        # self.color = color.red
+        # self.scale = (1, 2, 1)
+
+        # Додати меч як дочірню модель гравця
+        self.weapon = Entity(
+            model='sprite/weapon_sprite/oSword.obj',  # твоя модель меча
+            texture='sprite/weapon_sprite/tSword_Difusse.png',              # якщо буде текстура
+            parent=self,                              # прив'язка до гравця
+            position=(0.5, 1.5 , 1),                  # відносно гравця
+            scale=0.1,
+            rotation = (0, 80, 0)
+            #rotation_y = -80
+        )
+
+        
